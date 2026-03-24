ab14a4232c579625ebbb8e14760de43fbe2ae908
Dypel123 <kuta.michal24@gmail.com>
2026-03-24 17:38:43 +0100

    creepers and redstone primed tnt wont break vehicles

--- src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_NoExplosionDamage.java
+++ src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_NoExplosionDamage.java
@@ -5,10 +5,12 @@ import me.ryanhamshire.GPFlags.FlagManager;
 import me.ryanhamshire.GPFlags.GPFlags;
 import me.ryanhamshire.GPFlags.MessageSpecifier;
 import me.ryanhamshire.GPFlags.Messages;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.hanging.HangingBreakEvent;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
 
 public class FlagDef_NoExplosionDamage extends FlagDefinition {
 
@@ -38,6 +40,34 @@ public class FlagDef_NoExplosionDamage extends FlagDefinition {
         if (flag == null) return;
 
         e.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onVehicleDamage(VehicleDamageEvent event) {
+        Vehicle vehicle = event.getVehicle();
+
+        EntityDamageEvent last = vehicle.getLastDamageCause();
+
+        boolean isExplosion = false;
+
+        if (last != null) {
+            EntityDamageEvent.DamageCause cause = last.getCause();
+            if (cause == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION ||
+                    cause == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) {
+                isExplosion = true;
+            }
+        }
+        // to handle tnt primed using redstone
+        else if (event.getAttacker() == null) {
+            isExplosion = true;
+        }
+
+        if (!isExplosion) return;
+
+        Flag flag = this.getFlagInstanceAtLocation(vehicle.getLocation(), null);
+        if (flag == null) return;
+
+        event.setCancelled(true);
     }
 
     @Override
