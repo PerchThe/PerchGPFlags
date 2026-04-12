033635377ccebece87abd211269bb1f6346923f6
Lamburhts <kayalambrechts@gmail.com>
2026-04-12 12:57:43 +0200

    stuff

--- src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_ProtectNamedMobs.java
+++ src/main/java/me/ryanhamshire/GPFlags/flags/FlagDef_ProtectNamedMobs.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 import java.util.Arrays;
 import java.util.List;
@@ -45,6 +46,31 @@ public class FlagDef_ProtectNamedMobs extends FlagDefinition {
         Player player = (Player) damager;
         Claim claim = GriefPrevention.instance.dataStore.getClaimAt(entity.getLocation(), false, null);
         if (claim != null && claim.getPermission(player.getUniqueId().toString()) == ClaimPermission.Inventory) return;
+        event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onEnvironmentalDamage(EntityDamageEvent event) {
+        if (event instanceof EntityDamageByEntityEvent) return;
+
+        EntityDamageEvent.DamageCause cause = event.getCause();
+        if (cause != EntityDamageEvent.DamageCause.SUFFOCATION && cause != EntityDamageEvent.DamageCause.CRAMMING) {
+            return;
+        }
+
+        Entity entity = event.getEntity();
+
+        Flag flag = this.getFlagInstanceAtLocation(entity.getLocation(), null);
+        if (flag == null) return;
+        if (entity.getCustomName() == null) return;
+
+        EntityType eType = entity.getType();
+        if (eType == EntityType.PLAYER) return;
+        if (eType == EntityType.ARMOR_STAND) return;
+        if (eType == EntityType.ITEM_FRAME) return;
+        if (eType == EntityType.GLOW_ITEM_FRAME) return;
+        if (MythicMobsHook.isMythicMob(entity)) return;
+
         event.setCancelled(true);
     }
 
